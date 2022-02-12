8f3f0604618dba7671ba687cede7c8fdafcc8e63
buithuytien <buithuytien13.13@gmail.com>
2022-02-12 18:57:16 -0500

    scollable text

--- app/src/main/java/com/example/starverse/ZodiacActivity.java
+++ app/src/main/java/com/example/starverse/ZodiacActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class ZodiacActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class ZodiacActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zodiac);
+        TextView profile_text_description = findViewById(R.id.profile_text_description);
+        profile_text_description.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void openSignupPage(View view) {
