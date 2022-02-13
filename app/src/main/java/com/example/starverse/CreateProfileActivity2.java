76a8a5ba937cafa293d993a0bd142989850e5c62
buithuytien <buithuytien13.13@gmail.com>
2022-02-13 02:40:53 -0500

    abc

---
+++ app/src/main/java/com/example/starverse/CreateProfileActivity2.java
@@ -0,0 +1,14 @@
+package com.example.starverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateProfileActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile2);
+    }
+}
