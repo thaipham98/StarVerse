340269f81fd44f7fa75bc462aeb3f30e01d2605e
Thai G. Pham <tpham07@eecs.tufts.edu>
2022-02-13 06:15:42 -0500

    linked

--- app/src/main/java/com/example/starverse/ChatActivity.java
+++ app/src/main/java/com/example/starverse/ChatActivity.java
@@ -2,7 +2,9 @@ package com.example.starverse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+    }
+
+    public void openDashBoardPage(View view) {
+        Intent i = new Intent(this, DashboardActivity.class);
+        startActivity(i);
+    }
+
+    public void openChathubPage(View view) {
+        Intent i = new Intent(this, ChatMatchActivity.class);
+        startActivity(i);
+    }
+
+    public void openMyProfilePage(View view) {
+        Intent i = new Intent(this, MyProfileActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/starverse/DashboardActivity.java
+++ app/src/main/java/com/example/starverse/DashboardActivity.java
@@ -1,11 +1,10 @@
 package com.example.starverse;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
