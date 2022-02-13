dd4bd018dc7640c63e2737bee32fb3a1aa54fa4a
buithuytien <buithuytien13.13@gmail.com>
2022-02-13 06:05:20 -0500

    chatmatch

--- app/src/main/java/com/example/starverse/HelperNavBottom.java
+++ app/src/main/java/com/example/starverse/HelperNavBottom.java
@@ -1,19 +1,11 @@
 package com.example.starverse;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 
-public class DashboardActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-    }
+public class HelperNavBottom extends AppCompatActivity {
 
     public void openDashBoardPage(View view) {
         Intent i = new Intent(this, DashboardActivity.class);
@@ -29,4 +21,5 @@ public class DashboardActivity extends AppCompatActivity {
         Intent i = new Intent(this, MyProfileActivity.class);
         startActivity(i);
     }
+
 }
