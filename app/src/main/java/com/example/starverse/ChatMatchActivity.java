0771468fabdd6284b3db65da830ac1f385168082
buithuytien <buithuytien13.13@gmail.com>
2022-02-13 05:07:03 -0500

    chatmatch

---
+++ app/src/main/java/com/example/starverse/ChatMatchActivity.java
@@ -0,0 +1,14 @@
+package com.example.starverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_match);
+    }
+}
