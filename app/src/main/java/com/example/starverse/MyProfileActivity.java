8dcd72f6025ee47bf17b1b7e3ff3756a230e8ae9
buithuytien <buithuytien13.13@gmail.com>
2022-02-13 04:29:08 -0500

    Profile page

--- app/src/main/java/com/example/starverse/MyProfileActivity.java
+++ app/src/main/java/com/example/starverse/MyProfileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateProfileActivity2 extends AppCompatActivity {
+public class MyProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_profile2);
+        setContentView(R.layout.activity_my_profile);
     }
 }
